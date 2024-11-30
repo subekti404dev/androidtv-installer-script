15268eb539e0abee9cf2168208ecd1c9c5da5ce2
Urip Subekti <urip.subekti13@gmail.com>
2024-11-30 23:46:15 +0700

    add install apks

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Install !!!"
+echo "## Downloading Apks !!!"
+wget https://github.com/subekti404dev/androidtv-installer-script/raw/refs/heads/main/apps.zip -P /data/local/tmp
+
+echo "## Unzipping Apks !!!"
+mkdir /data/local/tmp/apps
+busybox unzip /data/local/tmp/apps.zip -d /data/local/tmp/apps
+
+echo "## Installing Apks !!!"
+find /data/local/tmp/apps -type f -name "*.apk" | while read apk; do
+    pm install -t -r $apk
+done
+
+echo "## Cleaning Up !!!"
+rm -rf /data/local/tmp/apps
+rm -rf /data/local/tmp/apps.zip
