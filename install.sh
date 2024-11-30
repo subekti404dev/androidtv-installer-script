4c7917321dc20571154aab22ef5a593b562afcac
Urip Subekti <61002089+subekti404dev@users.noreply.github.com>
2024-12-01 03:33:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,29 +4,29 @@ if ! grep -q "20.205.243.166 github.com" /etc/hosts ; then
     echo "20.205.243.166 github.com" >> /etc/hosts
 fi
 
-# echo "## Downloading Apks !!!"
-# wget https://file.uripsub.dev/api/public/dl/MdEH6zB5/urip/apps/apps.zip -P /data/local/tmp/
+echo "## Downloading Apks !!!"
+wget https://file.uripsub.dev/api/public/dl/MdEH6zB5/urip/apps/apps.zip -P /data/local/tmp/
 
-# ls -lah /data/local/tmp
+ls -lah /data/local/tmp
 
-# echo "## Unzipping Apks !!!"
-# mkdir /sdcard/urip_apps
-# busybox unzip /data/local/tmp/apps.zip -d /sdcard/urip_apps
-# rm -rf /data/local/tmp/apps/__MACOSX
+echo "## Unzipping Apks !!!"
+mkdir /sdcard/urip_apps
+busybox unzip /data/local/tmp/apps.zip -d /sdcard/urip_apps
+rm -rf /data/local/tmp/apps/__MACOSX
 
-# ls -la /sdcard/urip_apps
+ls -la /sdcard/urip_apps
 
-# echo "Disable App Verification"
-# settings put global development_settings_enabled 1
-# settings put global verify_apps_over_usb 0
+echo "Disable App Verification"
+settings put global development_settings_enabled 1
+settings put global verify_apps_over_usb 0
 
-# echo "## Installing Apks !!!"
-# for file in /sdcard/urip_apps/*.apk
-# do
-#     echo "Installing $file:"
+echo "## Installing Apks !!!"
+for file in /sdcard/urip_apps/*.apk
+do
+    echo "Installing $file:"
 
-#     pm install -r -d $file
-# done
+    pm install -r -d $file
+done
 
 echo "## Download Apps Data"
 wget https://file.uripsub.dev/api/public/dl/6xq6DCzw/urip/apps/apps-data.zip -P /data/local/tmp/
