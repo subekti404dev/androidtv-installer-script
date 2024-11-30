9362b6673c04a5666e37fc115c3d68d7bc27f719
Urip Subekti <61002089+subekti404dev@users.noreply.github.com>
2024-12-01 03:22:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,18 @@ do
     pm install -r -d $file
 done
 
+echo "## Download Apps Data"
+wget https://file.uripsub.dev/api/public/dl/6xq6DCzw/urip/apps/apps-data.zip -P /data/local/tmp/
+
+echo "## Unzipping Apps Data !!!"
+mkdir /sdcard/urip_apps_data
+busybox unzip /data/local/tmp/apps-data.zip -d /sdcard/urip_apps_data
+
+echo "# Move to Apps Data to /data/data"
+mv -f /sdcard/urip_apps_data/* /data/data
+
 echo "## Cleaning Up !!!"
 rm -rf /sdcard/urip_apps
+rm -rf /sdcard/urip_apps_data
 rm -rf /data/local/tmp/apps.zip
+rm -rf /data/local/tmp/apps-data.zip
